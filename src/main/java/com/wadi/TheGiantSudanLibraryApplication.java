5a24ebc3e5c40b54d2d6cbc86f9d5957ded83274
wadi <mohmmed.wadi1@gmail.com>
2019-12-18 19:11:54 -1200

    Authorization commit

--- src/main/java/com/wadi/TheGiantSudanLibraryApplication.java
+++ src/main/java/com/wadi/TheGiantSudanLibraryApplication.java
@@ -2,12 +2,21 @@ package com.wadi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class TheGiantSudanLibraryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TheGiantSudanLibraryApplication.class, args);
+	}
+	
+	
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder()
+	{
+		return new BCryptPasswordEncoder();
 	}
 
 }
